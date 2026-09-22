2abb017c7abb995b2019d94ef82233f0d9af43b5
vyshnavip1313 <vyshnavip1313@users.noreply.github.com>
2026-09-22 23:50:19 +0530

    Fix MinGit directory path in push_to_github.bat

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,6 +1,7 @@
 @echo off
-set "GIT_DIR=%LOCALAPPDATA%\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe"
-set "PATH=%GIT_DIR%\cmd;%GIT_DIR%\mingw64\bin;%PATH%"
+set "GIT_DIR="
+set "GIT_INSTALL_DIR=%LOCALAPPDATA%\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe"
+set "PATH=%GIT_INSTALL_DIR%\cmd;%GIT_INSTALL_DIR%\mingw64\bin;%PATH%"
 echo Pushing AgriConnect to GitHub: https://github.com/vyshnavip1313/agriconnect.git ...
 git push -u origin main
 echo.
