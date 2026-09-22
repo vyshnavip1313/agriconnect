c7bd623ac86f42cb7851df0dd451a21dc88d369f
vyshnavip1313 <vyshnavip1313@users.noreply.github.com>
2026-09-22 21:34:25 +0530

    Add helper scripts and documentation

---
+++ push_to_github.bat
@@ -0,0 +1,5 @@
+@echo off
+set "GIT=C:\Users\Mounika\AppData\Local\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe\cmd\git.exe"
+echo Pushing AgriLink to GitHub: https://github.com/vyshnavip1313/agrilink.git ...
+"%GIT%" push -u origin main
+pause
